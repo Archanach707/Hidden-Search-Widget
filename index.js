68779b6d4121c225cbfff1237b3efd1422272265
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-26 19:24:00 +0530

    implement on widget

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
 btn.addEventListener('click', () => {
+ 
   search.classList.toggle('active')
   input.focus()
 })
+
+input.addEventListener('blur', () => {
+  search.classList.remove('active')
+}); 
+
+
+
+
+
+
+
 
